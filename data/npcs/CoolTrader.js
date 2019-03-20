46c4a721d1af6723c9c3031e4604d8a65025a540
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-03-20 05:02:49 +0100

    Sample Trader Npc

---
+++ data/npcs/CoolTrader.js
@@ -0,0 +1,58 @@
+
+import core\npcs\moreEvents.js;
+
+function init(e){
+	yield init_event;
+}
+
+function load(e) {
+	yield load_event;
+}
+
+function interact(e){
+	yield interact_event;
+}
+
+function timer(e){
+	yield timer_event;
+}
+
+function tick(e){
+	yield tick_event;
+}
+
+function collide(e){
+	yield collide_event;
+}
+
+function damaged(e){
+	yield damaged_event;
+}
+
+function died(e){
+	yield died_event;
+}
+
+function kill(e){
+	yield kill_event;
+}
+
+function meleeAttack(e){
+	yield meleeAttack_event;
+}
+
+function rangedAttack(e){
+	yield rangedAttack_event;
+}
+
+function target(e){
+	yield target_event;
+}
+
+function targetLost(e){
+	yield targetLost_event;
+}
+
+function role(e){
+	yield role_event;
+}
