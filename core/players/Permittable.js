095755f1a21dbc8fea91817c7662d121ffb2d03b
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-03-28 21:57:08 +0100

    Worked on emotes

---
+++ core/players/Permittable.js
@@ -0,0 +1,22 @@
+//extendable function
+function Permittable(permDomain) {
+	this.permDomain = permDomain||this.type;
+	//Requires DataHandler
+	this.getPermission = function(){
+		return new Permission(this.getPermissionId());
+	};
+	this.getPermissionId = function(){
+		return this.permDomain+'.'+this.name;
+	};
+	this.onRemove(function(self, data) {
+		self.getPermission().remove(data); //Removes permission when DataHandler gets removed
+		
+	});
+	this.onSave(function(self, data){
+		var perm = self.getPermission();
+		if(!perm.exists(data)) {
+			//Create permission of permittable if not exists
+			perm.save(data); //this will run Permission onSave functions
+		}
+	});
+}
