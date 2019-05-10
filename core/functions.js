fbf83306672fcbf16157942b515bf36d831b8a66
slava_110 <unconfigured@null.spigotmc.org>
2019-05-10 19:32:48 +0300

    Added readDir and readFile functions for gui config reading

--- core/functions.js
+++ core/functions.js
@@ -8,6 +8,34 @@ var HAS_MOD_BACKPACKS = hasMCMod("backpacks16840");
 var UItem = (HAS_MOD_BACKPACKS ? Java.type("brad16840.common.UniqueItem") : null);
 var UItemInv = (HAS_MOD_BACKPACKS ? Java.type('brad16840.common.UniqueItemInventory') : null);
 
+//=========================FileUtils=========================
+var File = Java.type("java.io.File");
+var Files = Java.type("java.nio.file.Files");
+var Paths = Java.type("java.nio.file.Paths");
+
+function readDir(dirPath){
+	var res = [];
+	var files = new File(dirPath).listFiles();
+	for(var id in files as file){
+		if(file.isDirectory())
+			res = res.concat( readDir(file.toString()) );
+		else
+			res.push( Java.from( readFile(file.toString()) ).join("\n").replace(/\t/g, "  ") );
+	}
+	return res;
+}
+
+function readFile(filePath){
+	var path = Paths.get(filePath);
+	try{
+		var lines = Files.readAllLines(path, Java.type("java.nio.charset.StandardCharsets").UTF_8);
+		return lines;
+	} catch (e){
+		return [];
+	}
+}
+//===========================================================
+
 function objMerge(obj1, obj2, inheritNewProps=true){
     var obj3 = {};
     for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
