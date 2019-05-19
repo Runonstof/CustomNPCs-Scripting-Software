fa1ebeb31a5caf9826fa2f1f1e5baafd1290da28
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-05-19 20:37:38 +0200

    Changed custom build event

--- core/players/moreEvents.js
+++ core/players/moreEvents.js
@@ -1,6 +1,10 @@
 @block interact_event
 	(function(e){
 		//Create a 'build'-event for player scripts
+		var USABLE_BLOCKS = [
+			"minecraft:crafting_table",
+		];
+
 		if(e.type == 2) {
 			var place_block = e.player.getMainhandItem();
 			if( (place_block == null ? true : !place_block.isBlock()) ) { //If placeblock is null or not a block at all
@@ -8,8 +12,19 @@
 			}
 			if( (place_block == null ? false : place_block.isBlock()) ) { //Is place_block not null and is a block?
 				//Build event can be executed, check if it exists though
-				if(typeof(build) != typeof(undefined)) {
-					build(e, place_block);
+				//e.player.world.broadcast(e.target.getName());
+				if(
+					(
+						e.player.isSneaking()
+						||
+						(!e.target.isContainer() && USABLE_BLOCKS.indexOf(e.target.getName()) == -1)
+					)
+				) { //Trying to build
+					if(typeof(build) != typeof(undefined)) {
+						build(e, place_block);
+					}
+				} else { //Trying to interact
+					blockinteract(e);
 				}
 			} else {
 				if(typeof(blockinteract) != typeof(undefined)) {
