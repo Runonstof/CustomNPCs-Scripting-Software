26dfc50ac0d4afeddec265aaa3d235a5c73b025f
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-10 21:54:03 +0200

    Whitespace

--- core/players/executeCommand.js
+++ core/players/executeCommand.js
@@ -2,7 +2,7 @@ function executeCommand(player, command, as_player=null) {
 	if(as_player == null) { as_player = player.getName(); }
 	var API = Java.type('noppes.npcs.api.NpcAPI').Instance();
 	var cmd = API.createNPC(player.world.getMCWorld());
-	
+
 	return cmd.executeCommand("/execute "+as_player+" ~ ~ ~ "+command);
-	
+
 }
