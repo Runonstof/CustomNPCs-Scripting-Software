abcd0c7e18acae99177d23723ba5f2d0f667a32e
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-13 18:12:56 +0200

    Renamed ErrorHandling to ErrorHandler

---
+++ core/utils/ErrorHandler.js
@@ -0,0 +1,12 @@
+import core\utils\TellrawFormat.js;
+
+function handleError(error, logsToConsole=true, target=null) {
+    var world = API.getIWorld(0);
+    var errinfo = "$6Error in "+error.fileName+":"+error.lineNumber+"\n$e"+error.message+"$r\n\n"+error.stack+"\n"+error.getCause();
+
+    var errorTxt = "&cScript error in "+error.fileName+":"+error.lineNumber+"! &n&c[Error Info]{*|show_text:"+errinfo.replaceAll("&", "")+"}&r";
+    if(logsToConsole) {
+        print("Error in "+error.fileName+":"+error.lineNumber+"\n"+error.message+"\n\n"+error.stack);
+    }
+    executeCommandGlobal("/tellraw "+(target||"@a")+" "+strf(errorTxt));
+}
