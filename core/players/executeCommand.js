7d42676486efd9e56b614610cd4d8a3bd70f9a3f
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-13 18:12:34 +0200

    Added executeCommandGlobal(command) and import

--- core/players/executeCommand.js
+++ core/players/executeCommand.js
@@ -1,8 +1,13 @@
+import core\mods\noppes\Java.js;
+
 function executeCommand(player, command, as_player=null) {
 	if(as_player == null) { as_player = player.getName(); }
-	var API = Java.type('noppes.npcs.api.NpcAPI').Instance();
 	var cmd = API.createNPC(player.world.getMCWorld());
 
 	return cmd.executeCommand("/execute "+as_player+" ~ ~ ~ "+command);
 
+}
+
+function executeCommandGlobal(command) {
+	return API.createNPC(API.getIWorld(0).getMCWorld()).executeCommand(command);
 }
