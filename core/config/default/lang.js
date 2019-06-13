f377810146ab19a8eb0926527916ce586fc18eaf
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-13 15:54:55 +0200

    Added default config, started on config loading system

---
+++ core/config/default/lang.js
@@ -0,0 +1,22 @@
+//LANGUAGE settings
+var _MSG = {
+    //Error Strings
+	"cmdNotFound": "&cCould not find this command!",
+	"cmdNoPerm": "&cYou don't have permission to this command!",
+	"argNotValid": "&c'{argName}' is not a valid id/name! It can only contain: &o{allowed}",
+	"argToShort": "&c'{argName}' is too short! (Min. {allowed} characters)",
+	"argNoColor": "&c'{argName}' cannot contain colorcoding!",
+	"argEnum": "&c'{argName}' must be one of the following: &o{allowed}!",
+	"argNaN": "&c'{argName}' is not a number!",
+	"argMax": "&c'{argName}' cannot be bigger than {allowed}!",
+	"argMin": "&c'{argName}' cannot be smaller than {allowed}!",
+	"argNotExists": "&c{type} '{argVal}' does not exists!",
+	"argExists": "&c{type} '{argVal}' already exists!",
+	"argColor": "&cColors must be one of the following: {allowed}!",
+	"argColorEffect": "&cChat effects must be one of the following: {allowed}!",
+	"argItemAttr": "&cItem attributes must be one of these {allowed}!",
+	"argBool": "&c{dataType} must be true or false!",
+	//button texts
+    "undoBtnText": "Undo",
+    "refreshBtnText": "Refresh"
+};
