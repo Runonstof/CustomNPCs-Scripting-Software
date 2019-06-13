2456c695d87f04b3ed370f3b77f4600414f84af5
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-13 18:13:26 +0200

    Made a var of charset and added writeToFile(filePath, text)

--- core/utils/FileUtils.js
+++ core/utils/FileUtils.js
@@ -1,6 +1,7 @@
 var File = Java.type("java.io.File");
 var Files = Java.type("java.nio.file.Files");
 var Paths = Java.type("java.nio.file.Paths");
+var CHARSET_UTF_8 = Java.type("java.nio.charset.StandardCharsets").UTF_8;
 
 function readDir(dirPath){
 	var res = [];
@@ -17,9 +18,21 @@ function readDir(dirPath){
 function readFile(filePath){
 	var path = Paths.get(filePath);
 	try{
-		var lines = Files.readAllLines(path, Java.type("java.nio.charset.StandardCharsets").UTF_8);
+		var lines = Files.readAllLines(path, CHARSET_UTF_8);
 		return lines;
 	} catch (e){
 		return [];
+	}
+}
+
+function writeToFile(filePath, text, offset=null, length=null) {
+	var path = Paths.get(filePath);
+	try {
+		var writer = Files.newBufferedWriter(path, CHARSET_UTF_8);
+		writer.write(text, offset||0, length||text.length);
+
+		return true;
+	} catch (exc) {
+		return false
 	}
 }
