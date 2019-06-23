facd0c648441b69b3c2bec1b2235f1cb21aac712
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-23 13:03:53 +0200

    Added getItemType()

---
+++ core/mods/minecraft/Java.js
@@ -0,0 +1,30 @@
+import core\deobf\*.js;
+var MCItem = Java.type("net.minecraft.item.Item");
+var MCItemArmor = Java.type("net.minecraft.item.ItemArmor");
+var MCItemBow = Java.type("net.minecraft.item.ItemBow");
+var MCItemSword = Java.type("net.minecraft.item.ItemSword");
+var MCItemTool = Java.type("net.minecraft.item.ItemTool");
+var EntityEqSlot = Java.type("net.minecraft.inventory.EntityEquipmentSlot");
+
+function getItemType(iitemstack, mcentity=null) {
+    var mcitem = iitemstack.getMCItemStack()[MCP.ItemStack_getItem]();
+    if(mcitem instanceof MCItemBow) {
+        return "BOW";
+    }
+    if(mcitem instanceof MCItemSword) {
+        return "SWORD";
+    }
+    if(mcitem instanceof MCItemTool) {
+
+        return "TOOL";
+    }
+    if(mcitem instanceof MCItemArmor) {
+        if(mcentity != null) {
+            if(mcitem.isValidArmor(iitemstack.getMCItemStack(), EntityEqSlot.CHEST, mcentity)) {
+                return "ARMOR_CHEST";
+            }
+        }
+        return "ARMOR";
+    }
+    return "NONE";
+}
