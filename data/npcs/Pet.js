d97188c8033ef6b8ef8101752d733bd64af991e0
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-23 13:04:39 +0200

    Experimented with controlled npc flight

--- data/npcs/Pet.js
+++ data/npcs/Pet.js
@@ -1,68 +1,120 @@
-import core\functions.js;
-import core\npcs\moreEvents.js;
+import core\deobf\*.js;
+import core\math.js;
 
-var NPC = {
-	"OWNER": "",
-	"HP_BASE": 20,
-	"HP_INCR": 0.2, //Grows 1 in size every 5 levels
-	"SIZE_BASE": 2,
-	"SIZE_INCR": 0.2,
-	"LEVEL": 1,
-	"MAX_LEVEL": 255,
+
+
+//FLY SETTINGS
+//Pitch is 90 when player looks fully down
+var FLY_MIN_PITCH = 20; //(if on 20, then: when player pitch is above 20 or below -20 the npc will fly up/down)
+
+//Without these settings, controlled npc flight is very, very quirky
+	//I tested it all out, some I dont understand why, but tested with on/off and does make the difference
+var aiUpdate = {
+	//Set moving type to standing
+	"MovingType": 0,
+	//Set body rotation to manual
+	"StandingType": 1,
+	//Set stop on interact to false
+	"StopOnInteract": false,
+	//Set returns home to false
+	"ReturnsHome": false,
 };
 
-function init(e){
-	yield init_event;
+//Function to set the settings for an npc so they can fly/can be controlled properly
+function updateNpcSettings(npc) {
+	var npcAi = npc.getAi();
+
+	//For tick friendlyness, i hope, tick is hostile by itself
+	var hasChanged = false;
+
+	//If we are gonna set something (like standing type, returns home etc), we are gonna check if it is not already that, to make it tick-friendly
+	//(why set something to 2 when its already 2)
+
+	for(var a in aiUpdate) { //Loop all keys we wanna update
+		var au = aiUpdate[a];
+		if(npcAi["get"+a]() != au) { //if the key is not value we want
+			npcAi["set"+a](au); //Set the value
+			hasChanged = true;
+		}
+	}
+
+	var mcnpc = npc.getMCEntity();
+	var movespd = 15;
+	if(mcnpc[MCP.EntityLivingBase_getAIMoveSpeed]() != movespd) {
+		mcnpc[MCP.EntityLivingBase_setAIMoveSpeed](movespd);
+	}
+
+
+	//tick-friendlyness
+	if(hasChanged) {
+		npc.updateClient();
+		npc.reset();
+	}
+
 }
 
-function load(e) {
-	yield load_event;
+function npcSetNavType(npc, type) {
+	var npcAi = npc.getAi();
+	//Temp fix
+	var types = [
+		0,
+		1,
+		2,
+	];
+	var ti = types.indexOf(npcAi.getNavigationType());
+	while(npcAi.getNavigationType() != type) {
+		npcAi.setNavigationType(types[ti]);
+		ti = (ti+1 == types.length ? 0 : ti+1);
+	}
+
 }
 
-function interact(e){
-	yield interact_event;
+function init(e) {
+	updateNpcSettings(e.npc);
 }
 
-function timer(e){
-	yield timer_event;
+
+
+function interact(e) {
+	if(e.player.isSneaking()) { //let the dragon switch between flying/standing
+		var npcAi = e.npc.getAi();
+
+		npcSetNavType(e.npc, (e.npc.getAi().getNavigationType() == 1 ? 0 : 1));
+
+	} else {//if not sneaking
+		if(e.npc.getRiders().length == 0 && e.player.getMount() == null) {
+			//If npc has no riders and player is not sitting on something
+			e.npc.addRider(e.player);//set the player as rider
+		}
+
+	}
 }
 
-function tick(e){
-	yield tick_event;
-}
+function tick(e) {
+	updateNpcSettings(e.npc);
+	var riders = e.npc.getRiders();
+	for(var r in riders) { //Loop the riders on npc
+var rider = riders[r];
+		if(rider.getType() == 1) { //If the rider is an player
+			e.npc.setPitch(rider.getPitch());
+			e.npc.setRotation(rider.getRotation()); //Set rotation for fly-direction
 
-function collide(e){
-	yield collide_event;
-}
+			var mf = Math.round(rider.getMoveForward()); //Will be 1 when pressed W and -1 when S is pressed (or other key, depends on ur controls config)
+			var mv = Math.round(rider.getPitch()); //-90 when player looks up, 90 when player looks down
+			if(mf != 0) {
+				e.npc.setMoveForward(rider.getMoveForward());
 
-function damaged(e){
-	yield damaged_event;
-}
+				//e.npc.setMoveForward(rider.getMoveForward()); //Set moveForward of npc, the npc will moce in direction it looks
+				if(e.npc.getAi().getNavigationType() == 1) {
+					if(Math.abs(mv) > FLY_MIN_PITCH) {
+						e.npc.setMotionY(-sign(mv)/4);
+					}
+				}
+			} else if(e.npc.getMoveForward() != 0) {
+				e.npc.setMoveForward(0);
+			}
 
-function died(e){
-	yield died_event;
-}
-
-function kill(e){
-	yield kill_event;
-}
-
-function meleeAttack(e){
-	yield meleeAttack_event;
-}
-
-function rangedAttack(e){
-	yield rangedAttack_event;
-}
-
-function target(e){
-	yield target_event;
-}
-
-function targetLost(e){
-	yield targetLost_event;
-}
-
-function role(e){
-	yield role_event;
+			break;//exit for-loop
+		}
+	}
 }
