ab5b36d94370f4f84d8354fadfe55ed89e123bb1
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-06-23 13:03:11 +0200

    Added some funcs to deobf

---
+++ core/deobf/minecraft.js
@@ -0,0 +1,11 @@
+
+var MCP = {
+    "ItemStack_getItem": "func_77973_b",
+    "Item_getEquipmentSlot": "func_185083_B_",
+    "EntityLivingBase_travel": "func_191986_a",
+    "Entity_onGround": "field_70122_E",
+    "EntityLivingBase_setAIMoveSpeed": "func_70659_e",
+    "EntityLivingBase_getAIMoveSpeed": "func_70689_ay",
+    "EntityLiving_getMoveHelper": "func_70605_aq",
+    "EntityMoveHelper_strafe": "func_188488_a"
+};
