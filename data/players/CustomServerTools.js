9c5d0eb97b8f568d4fa1f76f40605b805ed889ee
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-07-29 00:03:17 +0200

    Tweaked chat()

--- data/players/CustomServerTools.js
+++ data/players/CustomServerTools.js
@@ -129,140 +129,143 @@ function broken(e) {
 
 function chat(e) {
 	yield chat_event;
-	var w = e.player.world;
-	var data = w.getStoreddata();
-	var sb = w.getScoreboard();
 
-	var dpl = new Player(e.player.getName()).init(data);
-	var allwdcolors = dpl.getAllowedColors(data, sb);
-	var esccolors = removeFromArray(_RAWCODES, allwdcolors);
-	var escmsg = escCcs(e.message.toString(), esccolors);
+	if(!e.isCanceled()) {
+		var w = e.player.world;
+		var data = w.getStoreddata();
+		var sb = w.getScoreboard();
 
-	var logMsg = "["+e.player.getName()+"] -> "+escCcs(e.message.toString());
+		var dpl = new Player(e.player.getName()).init(data);
+		var allwdcolors = dpl.getAllowedColors(data, sb);
+		var esccolors = removeFromArray(_RAWCODES, allwdcolors);
+		var escmsg = escCcs(e.message.toString(), esccolors);
 
-	var prefcol = dpl.getChatColorPref(sb, data);
-	var chats = dpl.getChats(data);
+		var logMsg = "["+e.player.getName()+"] -> "+escCcs(e.message.toString());
 
-	if(!dpl.canCreateCommandText(data, sb)) {
-		escmsg = escmsg.replace(CHAT_CMD_RGX_G, '');
-	}
+		var prefcol = dpl.getChatColorPref(sb, data);
+		var chats = dpl.getChats(data);
 
-	//Check @, $ and # mentions
-	//@ - Player
-	//$ - Team
-	//# - Chatchannel
+		if(!dpl.canCreateCommandText(data, sb)) {
+			escmsg = escmsg.replace(CHAT_CMD_RGX_G, '');
+		}
 
-	var players = w.getAllPlayers();
-	var teams = sb.getTeams();
-	var allChats = new ChatChannel().getAllDataIds(data);
-	var prgx = /@([\w\-]+)/g;
-	var trgx = /\$([\w\-\.]+)/g;
-	var crgx = /#([\w\-\.]+)/g;
-	var mentioned = [];
+		//Check @, $ and # mentions
+		//@ - Player
+		//$ - Team
+		//# - Chatchannel
 
-	var tmatch = escmsg.match(trgx) || [];
-	for(var tm in tmatch as tmt) {
-		for(var tmi in teams as team) {
-			if(occurrences(team.getName().toLowerCase(), tmt.replace(trgx, '$1').toLowerCase()) > 0) {
-				for(var ply in players as plyr) {
-					var pteam = sb.getPlayerTeam(plyr.getName());
-					if(pteam != null) {
-						if(pteam.getName() == team.getName()) {
-							if(mentioned.indexOf(plyr.getName()) == -1) {
-								plyr.sendNotification(ccs('&'+getColorId(team.getColor())+'&o$'+team.getName()), ccs(dpl.getNameTag(sb)), 2);
-								mentioned.push(plyr.getName());
+		var players = w.getAllPlayers();
+		var teams = sb.getTeams();
+		var allChats = new ChatChannel().getAllDataIds(data);
+		var prgx = /@([\w\-]+)/g;
+		var trgx = /\$([\w\-\.]+)/g;
+		var crgx = /#([\w\-\.]+)/g;
+		var mentioned = [];
+
+		var tmatch = escmsg.match(trgx) || [];
+		for(var tm in tmatch as tmt) {
+			for(var tmi in teams as team) {
+				if(occurrences(team.getName().toLowerCase(), tmt.replace(trgx, '$1').toLowerCase()) > 0) {
+					for(var ply in players as plyr) {
+						var pteam = sb.getPlayerTeam(plyr.getName());
+						if(pteam != null) {
+							if(pteam.getName() == team.getName()) {
+								if(mentioned.indexOf(plyr.getName()) == -1) {
+									plyr.sendNotification(ccs('&'+getColorId(team.getColor())+'&o$'+team.getName()), ccs(dpl.getNameTag(sb)), 2);
+									mentioned.push(plyr.getName());
+								}
 							}
 						}
 					}
-				}
-				escmsg = escmsg.replace(tmt, '&'+getColorId(team.getColor())+'&o$'+team.getName()+prefcol);
-			}
-		}
-	}
-
-	var pmatch = escmsg.match(prgx) || [];
-	for(var pm in pmatch as pmt) {
-		for(var ply in players as plyr) {
-			if(occurrences(plyr.getName().toLowerCase(), pmt.replace(prgx, '$1').toLowerCase()) > 0) {
-				var pmpl = new Player(plyr.getName()); //Dont have to init, only using scoreboard
-				if(mentioned.indexOf(plyr.getName()) == -1) {
-					plyr.sendNotification(ccs("&9&l&o@"+plyr.getName()), ccs(dpl.getNameTag(sb)), 2);
-					mentioned.push(plyr.getName());
-				}
-				escmsg = escmsg.replace(pmt, '&9&o@'+plyr.getName()+'{suggest_command:/msg '+plyr.getName()+' |show_text:'+pmpl.getNameTag(sb, '', '', '', '$')+'}'+prefcol);
-			}
-		}
-	}
-
-
-	var cmatch = escmsg.match(crgx) || [];
-	for(var cm in cmatch as cmt) {
-		for(var cmi in allChats as chat) {
-			chat = new ChatChannel(chat).init(data);
-			var cmm = cmt.replace(crgx, '$1');
-			var cmp = chat.getPermission(data);
-			if(cmp.permits(e.player.getName(), sb, data)) {
-				if(occurrences(chat.name.toLowerCase(), cmm.toLowerCase()) > 0 || occurrences(chat.data.displayName.toLowerCase(), cmm.toLowerCase()) > 0) {
-					escmsg = escmsg.replace(cmt, '&'+getColorId(chat.data.color)+'&l#'+chat.data.displayName+'{run_command:!chat list '+chat.name+'}'+prefcol);
+					escmsg = escmsg.replace(tmt, '&'+getColorId(team.getColor())+'&o$'+team.getName()+prefcol);
 				}
 			}
 		}
-	}
 
-	//Chat emotes
-	escmsg = parseEmotes(escmsg, dpl.getAllowedEmotes(sb, data));
-
-	var pbounty = 0;
-	var pobj = sb.getObjective("bounty");
-	if(pobj != null) {
-		pbounty = pobj.getScore(e.player.getName()).getValue();
-	}
-	//time
-	var curTimeStr = new Date().toLocaleTimeString("us-US", {
-		timeZone: "America/New_York"
-	}).split(":");
-	curTimeStr.pop();
-	curTimeStr = curTimeStr.join(":");
-
-	//Concat new message
-	var hoverInfo = '$6$lBounty: $r:money:$e'+getAmountCoin(pbounty)+"\n";
-	var newmsg = "["+curTimeStr+"] "+parseEmotes(dpl.getNameTag(sb, ' -> ', '{suggest_command:@'+dpl.name+' |show_text:'+hoverInfo+'}', '', '&', data))+prefcol+escmsg;
-	var toldPlayers = [];
-	var wp = w.getAllPlayers();
+		var pmatch = escmsg.match(prgx) || [];
+		for(var pm in pmatch as pmt) {
+			for(var ply in players as plyr) {
+				if(occurrences(plyr.getName().toLowerCase(), pmt.replace(prgx, '$1').toLowerCase()) > 0) {
+					var pmpl = new Player(plyr.getName()); //Dont have to init, only using scoreboard
+					if(mentioned.indexOf(plyr.getName()) == -1) {
+						plyr.sendNotification(ccs("&9&l&o@"+plyr.getName()), ccs(dpl.getNameTag(sb)), 2);
+						mentioned.push(plyr.getName());
+					}
+					escmsg = escmsg.replace(pmt, '&9&o@'+plyr.getName()+'{suggest_command:/msg '+plyr.getName()+' |show_text:'+pmpl.getNameTag(sb, '', '', '', '$')+'}'+prefcol);
+				}
+			}
+		}
 
 
-	if(chats.length > 0) {
-		for(var c in chats as ch) {
+		var cmatch = escmsg.match(crgx) || [];
+		for(var cm in cmatch as cmt) {
+			for(var cmi in allChats as chat) {
+				chat = new ChatChannel(chat).init(data);
+				var cmm = cmt.replace(crgx, '$1');
+				var cmp = chat.getPermission(data);
+				if(cmp.permits(e.player.getName(), sb, data)) {
+					if(occurrences(chat.name.toLowerCase(), cmm.toLowerCase()) > 0 || occurrences(chat.data.displayName.toLowerCase(), cmm.toLowerCase()) > 0) {
+						escmsg = escmsg.replace(cmt, '&'+getColorId(chat.data.color)+'&l#'+chat.data.displayName+'{run_command:!chat list '+chat.name+'}'+prefcol);
+					}
+				}
+			}
+		}
+
+		//Chat emotes
+		escmsg = parseEmotes(escmsg, dpl.getAllowedEmotes(sb, data));
+
+		var pbounty = 0;
+		var pobj = sb.getObjective("bounty");
+		if(pobj != null) {
+			pbounty = pobj.getScore(e.player.getName()).getValue();
+		}
+		//time
+		var curTimeStr = new Date().toLocaleTimeString("us-US", {
+			timeZone: "America/New_York"
+		}).split(":");
+		curTimeStr.pop();
+		curTimeStr = curTimeStr.join(":");
+
+		//Concat new message
+		var hoverInfo = '$6$lBounty: $r:money:$e'+getAmountCoin(pbounty)+"\n";
+		var newmsg = "["+curTimeStr+"] "+parseEmotes(dpl.getNameTag(sb, ' -> ', '{suggest_command:@'+dpl.name+' |show_text:'+hoverInfo+'}', '', '&', data))+prefcol+escmsg;
+		var toldPlayers = [];
+		var wp = w.getAllPlayers();
+
+
+		if(chats.length > 0) {
+			for(var c in chats as ch) {
+				for(var ww in wp as wpl) {
+					if(toldPlayers.indexOf(wpl.getName()) == -1 && ch.data.players.indexOf(wpl.getName()) > -1) {
+						var wchats = [];
+						var wcnames = [];
+						new Player(wpl.getName()).init(data).getChats(data).forEach(function(wchat){
+							wchats.push(wchat.getTag('', '$'));
+							wcnames.push(wchat.name);
+						});
+						var ccpref = parseEmotes('['+curTimeStr+']&l[:lang:]{run_command:!chat list '+wcnames.join(" ")+'|show_text:'+wchats.join("\n")+'}&r ');
+						executeCommand(wpl, "/tellraw "+wpl.getName()+" "+strf(ccpref+newmsg));
+						toldPlayers.push(wpl.getName());
+					}
+				}
+			}
+
+
+		} else {
 			for(var ww in wp as wpl) {
-				if(toldPlayers.indexOf(wpl.getName()) == -1 && ch.data.players.indexOf(wpl.getName()) > -1) {
-					var wchats = [];
-					var wcnames = [];
-					new Player(wpl.getName()).init(data).getChats(data).forEach(function(wchat){
-						wchats.push(wchat.getTag('', '$'));
-						wcnames.push(wchat.name);
-					});
-					var ccpref = parseEmotes('['+curTimeStr+']&l[:lang:]{run_command:!chat list '+wcnames.join(" ")+'|show_text:'+wchats.join("\n")+'}&r ');
-					executeCommand(wpl, "/tellraw "+wpl.getName()+" "+strf(ccpref+newmsg));
+				var wplo = new Player(wpl.getName()).init(data);
+				if(toldPlayers.indexOf(wpl.getName()) == -1 && wplo.getChats(data).length == 0) {
+
+
+					executeCommand(wpl, "/tellraw "+wpl.getName()+" "+strf(newmsg));
 					toldPlayers.push(wpl.getName());
 				}
 			}
 		}
 
-
-	} else {
-		for(var ww in wp as wpl) {
-			var wplo = new Player(wpl.getName()).init(data);
-			if(toldPlayers.indexOf(wpl.getName()) == -1 && wplo.getChats(data).length == 0) {
-
-
-				executeCommand(wpl, "/tellraw "+wpl.getName()+" "+strf(newmsg));
-				toldPlayers.push(wpl.getName());
-			}
-		}
+		Logger.info(logMsg);
+		e.setCanceled(true); //Cancel real message
 	}
-
-	Logger.info(logMsg);
-	e.setCanceled(true); //Cancel real message
 }
 
 function containerOpen(e) {
