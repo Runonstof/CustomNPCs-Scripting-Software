688e79cf77e5c58d058535bacd0796e1bb2e7e1f
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-07-29 00:02:22 +0200

    rawformatting

--- core/utils/World.js
+++ core/utils/World.js
@@ -1,5 +1,7 @@
 import core\mods\noppes\Java.js;
+import core\utils\TellrawFormat.js;
 
+//
 function worldOut(str) {
-    API.getIWorld(0).broadcast(str);
+    API.getIWorld(0).broadcast(strf(str));
 }
