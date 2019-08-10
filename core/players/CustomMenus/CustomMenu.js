d9d8482af3b00e69467460576ceb9acc2c8505ae
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-08-11 00:54:44 +0200

    Added new custom menu handler

---
+++ core/players/CustomMenus/CustomMenu.js
@@ -0,0 +1,239 @@
+import core\mods\noppes\Java.js;
+
+import core\mods\noppes\INbt.js;
+
+import core\players\executeCommand.js;
+
+import core\xcommandsAPI.js;
+
+import core\utils\FileUtils.js;
+
+var MENU_TIMER_ID = 420;
+var MENU_TIMER_PAYLOAD = null;
+var MENU_ON_CLOSE = [];
+
+@block timer_event
+    if(e.id == MENU_TIMER_ID) {
+        if(MENU_TIMER_PAYLOAD) {
+            //Open new menu
+            var menu = new CustomMenu().fromJson(MENU_TIMER_PAYLOAD.menu, MENU_TIMER_PAYLOAD.data);
+
+            menu.open(e.player);
+            MENU_TIMER_PAYLOAD = null;
+        }
+    }
+@endblock
+
+@block customChestClosed_event
+    for(var i in MENU_ON_CLOSE as closeEvent) {
+        try {
+            closeEvent.fn.apply(closeEvent.self, [e,closeEvent.data||{}]);
+        } catch(exc) {
+            handleError(exc, true, e.player.getName());
+        }
+    }
+    MENU_ON_CLOSE = [];
+@endblock
+
+@block customChestClicked_event
+    var snbt = e.slotItem.getNbt();
+    if(snbt.getBoolean("takeable")) {
+        var heldItem = e.heldItem.copy();
+        e.heldItem = e.slotItem;
+        e.slotItem = heldItem;
+    }
+
+    if(snbt.has("onClick")) {
+        var clickEvents = Java.from(nbtGetList(snbt, "onClick"));//map parse json
+        handleMenuEvents(e.player, clickEvents.map(function(ce){ return JSON.parse(ce); }));
+    }
+
+@endblock
+
+function handleMenuEvents(player, evs) {
+    for(var c in evs as ev) {
+        switch(ev.type) {
+            case "run_command":
+                executeCommand(player, ev.meta.command||"");
+                break;
+            case "run_xcommand":
+            case "run_xcommand_admin":
+                executeXCommand(ev.meta.command||"", player, ev.type === "run_xcommand");
+                break;
+            case "run_file":
+                var scrPath = ev.meta.file||"";
+                var scrFile = new File(scrPath);
+                if(scrFile.exists()) {
+                    var scr = readFileAsString(scrPath);
+                    var scrFunc = new Function('e', 'payload', scr);
+                    var payl = objMerge({
+                        //defaults
+                    }, ev.meta.payload||{});
+
+                    try {
+                        scrFunc(e, payl);
+                    } catch(exc) {
+                        handleError(exc, false, player.getName());
+                    }
+                } else {
+                    tellPlayer(player, "&cFile '"+scrPath+"' doesn't exist!");
+                }
+                break;
+            case "run_script":
+                var scr = ev.meta.script||"";
+                var scrFunc = new Function('e', 'payload', scr);
+                var payl = objMerge({
+                    //defaults
+                }, ev.meta.payload||{});
+
+                try {
+                    scrFunc(e, payl);
+                } catch(exc) {
+                    handleError(exc, false, player.getName());
+                }
+                break;
+            case "open_menu":
+                var menuPath = ev.meta.file||"";
+                var menuFile = new File(menuPath);
+                if(menuFile.exists()) {
+                    var menuFileText = readFileAsString(menuPath);
+                    var menuJson = null;
+
+                    try {
+                        menuJson = JSON.parse(menuFileText);
+                    } catch (exc) {
+                        handleError(exc, false, player.getName());
+                    }
+
+                    if(menuJson !== null) {
+                        player.closeGui();
+                        MENU_TIMER_PAYLOAD = {'menu':menuJson,'data':ev.meta.data||{},'onClose':menuJson.onClose||[]};
+                        player.timers.forceStart(MENU_TIMER_ID, 1, false);
+
+                    }
+
+
+                } else {
+                    tellPlayer(player, "&cFile '"+menuPath+"' does not exists!")
+                }
+                break;
+            case "cancel":
+                if(e.isCancelable()) {
+                    e.setCanceled(true);
+                }
+                break;
+            case "close_menu":
+                player.closeGui();
+                break;
+        }
+    }
+}
+
+function CustomMenu(name) {
+    this.name = name||"";
+    this.rows = 6;
+    this.items = [];
+
+    this.fromJson = function(json) {
+        this.name = json.name||"";
+        this.rows = Math.min(Math.max(json.rows||6, 1),6);
+        if(json.items) {
+            for(var i in json.items as jitem) {
+                this.items.push(new CustomMenuItem().fromJson(jitem));
+            }
+        }
+
+        return this;
+    };
+
+    this.onClose = function(fn) {
+        MENU_ON_CLOSE.push({"fn":fn,"self":this});
+    };
+
+    this.getItems = function() {
+        return new Collection(this.items);
+    };
+
+    this.open = function(player) {
+        var container =  player.showChestGui(this.rows);
+        this.populate(player.world, container);
+        return container;
+    };
+
+    this.update = function(){
+        this.getItems().do("each",function(item){item.update();});
+    };
+
+    this.populate = function(w, container) {
+        container.setName(parseEmotes(ccs(this.name)));
+        var items = this.items;
+        for(var i in this.items as citem) {
+            container.setSlot(36+citem.slot, citem.toItemStack(w));
+        }
+    };
+}
+
+function CustomMenuItem(id, damage=0, count=1) {
+    this.id = id;
+    this.lore = [];
+    this.slot = 0;
+    this.count = count;
+    this.damage = damage;
+    this.nbt = null;
+    this.classes = [];
+    this.name = null;
+    this.takeable = false;
+
+    this.fromJson = function(json){
+        this.id=json.id;
+        if("name" in json) {
+            this.name = json.name;
+        }
+        this.lore = json.lore||[];
+        this.damage = json.damage||0;
+        this.count = json.count||1;
+        this.nbt = json.nbt||null;
+        this.classes = json.classes||[];
+        this.takeable = json.takeable||false;
+        this.slot = json.slot||0;
+        this.onClickFuncs = json.onClick||[];
+
+        return this;
+    };
+
+    this.onClickFuncs = [];
+
+    this.onClick = function(type, meta){
+        this.onClickFuncs.push({
+            "type": type,
+            "meta": meta
+        });
+    };
+
+    this.fromItemStack = function(stack) {
+        this.id = stack.getName();
+        this.damage = stack.getItemDamage();
+
+        return this;
+    };
+    this.toItemStack = function(w){
+        var item = w.createItem(this.id, this.damage, this.count);
+        item.setLore(this.lore.map(function(l){
+            return parseEmotes(ccs(l));
+        }));
+        if(this.name) {
+            item.setCustomName(parseEmotes(ccs(this.name||"")));
+        }
+        var inbt = item.getNbt();
+        inbt.setList("classes", (Java.from(inbt.getList("classes", inbt.getListType("classes")))||[]).concat.apply([], [this.classes]));
+        inbt.setList("onClick", (Java.from(inbt.getList("onClick", inbt.getListType("onClick")))||[]).concat.apply([], [this.onClickFuncs.map(function(f){return JSON.stringify(f);})]));
+        inbt.setBoolean("takeable", this.takeable);
+
+
+
+        if(this.nbt) {
+            inbt.merge(API.stringToNbt(JSON.stringify(this.nbt)));
+        }
+        return item;
+    };
+}
