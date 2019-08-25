48bc4b4450bbd9ce9bf100c2f398b15b2513f7e0
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-08-25 17:46:14 +0200

    handleError() more flexible

--- core/utils/ErrorHandler.js
+++ core/utils/ErrorHandler.js
@@ -2,7 +2,17 @@ import core\utils\TellrawFormat.js;
 
 function handleError(error, logsToConsole=true, target=null) {
     var world = API.getIWorld(0);
-    var errinfo = "$6Error in "+error.fileName+":"+error.lineNumber+"\n$e"+error.message+"$r\n\n"+error.stack+"\n";
+    var errinfo = "";
+    if(error.fileName) {
+        errinfo += "$6Error in "+error.fileName+(error.lineNumber?error.lineNumber:"")+"\n";
+    }
+    if(error.message) {
+        errinfo += "$e"+error.message.replaceAll("&", "")+"\n";
+    }
+    if(error.stack) {
+        errinfo += "$r\n"+error.stack+"\n";
+    }
+    errinfo = "$6Error in "+error.fileName+":"+error.lineNumber+"\n$e"+error.message+"$r\n\n"+error.stack+"\n";
 
     var errorTxt = "&cScript error in "+error.fileName+":"+error.lineNumber+"! &n&c[Error Info]{*|show_text:"+errinfo.replaceAll("&", "")+"}&r";
     if(logsToConsole) {
