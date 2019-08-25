5aa177bc6c10f2060736349d01c8459b14d8c59b
Runonstof <36049776+Runonstof@users.noreply.github.com>
2019-08-25 17:44:56 +0200

    Changed CST Config

--- core/config/default/server.js
+++ core/config/default/server.js
@@ -11,11 +11,22 @@ var CONFIG_SERVER = {
 	],
 	"DEFAULT_PERM_PLAYERS": [],
 	"DEFAULT_TEAM_JOIN": "Player",
-	"ENABLE_BOT_MEE6": true,
-	"DEFAULT_PAY": 0,
-	"DEFAULT_MONEY": 0,
-	"MONEY_POUCH_LOSE_PERC": 50,
-	"COINTABLE": {},
+	"DEVELOPMENT_MODE": false,
+	"USE_DISK": "DEFAULT",
+	"LICENSE_KEY": "",
+	"FILE_DISKS": {
+		"DEFAULT": {
+			"path": "{worldname}/customnpcs/scripts/world_data.json",
+		},
+		"CST_DATA": {
+			"path": "CustomServerTools/data/data.json",
+		}
+	}
+	///"ENABLE_BOT_MEE6": true,
+	//"DEFAULT_PAY": 0,
+	//"DEFAULT_MONEY": 0,
+	//"MONEY_POUCH_LOSE_PERC": 50,
+	//"COINTABLE": {},
 };
 
 var DEFAULT_MONEY = 0;
